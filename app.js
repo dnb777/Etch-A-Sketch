3efbf9a9db88b9ef558cbc4e191cbb0081a91da4
Damian Bruno <damianbruno17@gmail.com>
2022-04-25 01:13:05 -0300

    Added paintSquare and resetGrid functions to paint the divs when moving the cursor and to clear the grid respectively

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 "use strict"
 
 const container = document.querySelector("#container");
-
-
-
+const button = document.querySelector("#btn");
 
 
 function addSquares(n) {
 	for (let i = 0; i < (n * n); i++){
 		const div = document.createElement("div");
-		div.classList.add("squares");
+		div.classList.add('square');
 		container.appendChild(div);
 	};
 };
 
-addSquares(16)
+
+addSquares(32)
+
+const divs = container.querySelectorAll('.square');
+const paintSquare = divs.forEach((div) => {
+	div.addEventListener('mousemove', () => {
+		div.classList.add('black');
+	});
+});
+
+const resetGrid = button.addEventListener('click', () => {
+	divs.forEach((div) => {
+		div.classList.remove('black');
+	});
+});
