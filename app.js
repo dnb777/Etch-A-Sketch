4207f1e04f67e6a912fc04fadc17bc524655dea7
Damian Bruno <damianbruno17@gmail.com>
2022-04-25 01:50:08 -0300

    Added function to ask user for input to set the size of the grid

--- app.js
+++ app.js
@@ -2,18 +2,27 @@
 
 const container = document.querySelector("#container");
 const button = document.querySelector("#btn");
+const input = getGridSize();
 
+function getGridSize() {
+	let input = 0;
+	while(true) {
+		input = prompt("How many squares per side do you want?", 0);
+		if (input <= 100) break;
+		alert("To large");
+	}
+	return input;
+};
 
 function addSquares(n) {
-	for (let i = 0; i < (n * n); i++){
+	for (let i = 0; i < (input * input); i++){
 		const div = document.createElement("div");
 		div.classList.add('square');
 		container.appendChild(div);
 	};
 };
 
-
-addSquares(32)
+addSquares();
 
 const divs = container.querySelectorAll('.square');
 const paintSquare = divs.forEach((div) => {
@@ -27,3 +36,7 @@ const resetGrid = button.addEventListener('click', () => {
 		div.classList.remove('black');
 	});
 });
+
+
+
+
