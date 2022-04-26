bdbcc1421620de9ffc3d6dfe2c5108294860a6a2
Damian Bruno <damianbruno17@gmail.com>
2022-04-26 00:09:12 -0300

    Added defaultGrid function to set an inial grid ready to draw and paintSquare function to not repeat the same code

--- app.js
+++ app.js
@@ -5,19 +5,6 @@ const button = document.querySelector("#btn");
 const root = document.documentElement;
 
 
-function getGridSize() {
-	let userInput = 0;
-	while(true) {
-		userInput = prompt("How many squares per side do you want?", 0);
-		if (userInput <= 100) break;
-		alert("To large");
-	}
-	userInput = Number(userInput);
-	root.style.setProperty('--col', userInput);
-	root.style.setProperty('--row', userInput);
-	return userInput;
-};
-
 function addSquares(n) {
 	for (let i = 0; i < (n * n); i++){
 		const div = document.createElement("div");
@@ -26,20 +13,46 @@ function addSquares(n) {
 	};
 };
 
-const resetGrid = button.addEventListener('click', () => {
-	const input = getGridSize();
-	const newGrid = addSquares(input);
-	
+//Set the inicial grid to 16x16
+function defaultGrid() {
+	addSquares(16);
 	const divs = container.querySelectorAll('.square');
-	divs.forEach((div) => {
-		div.classList.remove('black');
-	});
+	paintSquares(divs);
+}
+defaultGrid();
 
-	const paintSquare = divs.forEach((div) => {
+function paintSquares(divs) {
+	divs.forEach((div) => {
 		div.addEventListener('mousemove', () => {
 			div.classList.add('black');
 		});
 	});
+};
+
+//Ask user for input to set the scale of the grid, input has to be <= 100
+function getGridSize() {
+	let userInput = 0;
+	while(true) {
+		userInput = prompt("How many squares per side do you want?", 0);
+		if (userInput <= 100) break;
+		alert("To large");
+	}
+	userInput = Number(userInput);
+	root.style.setProperty('--col', userInput);//change the value of grid-template-column
+	root.style.setProperty('--row', userInput);//change the value of grid-template-row
+	return userInput;
+};
+
+const resetGrid = button.addEventListener('click', () => {
+	const input = getGridSize();
+	const newGrid = addSquares(input);
+	
+	//reset the background-color of the div's in the grid
+	const divs = container.querySelectorAll('.square');
+	divs.forEach((div) => {
+		div.classList.remove('black');
+	});
+	paintSquares(divs);
 });
 
 
